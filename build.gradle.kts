eefdb11b48acd57a92ce7165cffb16a413b3b9b8
trevarmand <armand.t@northeastern.edu>
2021-06-12 12:19:27 -0400

    Git ignore build and idea project folders

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.32"
+    application
+}
+
+group = "me.owner"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("twoCars.TwoCarsKt")
+}
+
